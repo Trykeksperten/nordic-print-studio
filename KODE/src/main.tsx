830f81068b8964dc1c9c91ae13c88bbd43a08b03
Joakim Sarsgaard <joakimsarsgaard@MacBook-Pro.local>
2026-04-09 15:26:25 +0200

    Trigger fresh frontend bundle for cache bust

--- KODE/src/main.tsx
+++ KODE/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+(window as Window & { __TRYKEKSPERTEN_BUILD__?: string }).__TRYKEKSPERTEN_BUILD__ = "2026-04-09-2";
+
 createRoot(document.getElementById("root")!).render(<App />);
